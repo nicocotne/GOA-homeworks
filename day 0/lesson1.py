0aa9df98c3a1dc244110f0003781daa8d6b256f8
nicocotne <nicoocingm@gmil.com>
2024-01-20 07:56:14 -0800

    first commit

---
+++ day 0/lesson1.py
@@ -0,0 +1,9 @@
+name = "Im cotne"
+surname = "magradze."
+age = "Im 14 years old"
+gender = "boy."
+height = "me height is 178.8 cm."
+knows_programing = True
+is_ugly = False
+isUgly = False
+print(name + " " + surname + " " + age + " " + gender + " " + height)
