8ad21258476109ab3aabbb17d823f3482c195704
nicocotne <nicoocingm@gmail.com>
2024-01-20 22:27:35 -0800

    first commit

--- day 0/lesson1.py
+++ day 0/lesson1.py
@@ -7,3 +7,4 @@ knows_programing = True
 is_ugly = False
 isUgly = False
 print(name + " " + surname + " " + age + " " + gender + " " + height)
+print("Im cotne magradze. Im 14 years old boy. me height is 178.8 cm.")
