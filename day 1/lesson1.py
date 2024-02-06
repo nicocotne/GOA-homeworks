0921c2259dbf646603ee7eefaa295c81ad426358
nicocotne <nicoocingm@gmail.com>
2024-02-06 08:45:07 -0800

    second commit

--- day 1/lesson1.py
+++ day 1/lesson1.py
@@ -6,5 +6,5 @@ height = "me height is 178.8 cm."
 knows_programing = True
 is_ugly = False
 isUgly = False
-print(name + " " + surname + " " + age + " " + gender + " " + height)
+print(name, surname,  age,  gender,  height,)
 print("Im cotne magradze. Im 14 years old boy. me height is 178.8 cm.")
---
+++ day 2/lesson2.py
@@ -0,0 +1,3 @@
+print("gavicani chemi jgufelebi da raodenime warmatebuli moswavle")
+print("me momewona chemi jgufelebis megobruloba da sikete da kide me momewona mentoris mems")
+print("chemi pirveli da saintereso gakvetili goa shi")
