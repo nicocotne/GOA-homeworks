20b3de9de9a0425e224e9fbda11b898d0ad5e6e9
nicocotne <157065078+nicocotne@users.noreply.github.com>
2024-02-09 21:27:16 -0800

    Update homework3.1.py

--- day 3/homework3.1.py
+++ day 3/homework3.1.py
@@ -1,4 +1,10 @@
-from turtle import *
-
-
-width('1 and 72 * 12)
+name  "Im cotne"
+surname = "magradze.
+a ge  Im 14 years old"
+gnder = "boy."
+height = "me height is 178.8 cm.
+knows_programing  True
+is_ugly False
+isUgly False
+printname, surname,  age  gnder  height,)
+print"Im cotne magradze. Im 14 years old boy. me height is 178.8 cm.")
