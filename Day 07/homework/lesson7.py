d251ef6093586ddddd1436b285fd3f21d034d476
nicocotne <nicoocingm@gmail.com>
2024-02-24 05:19:17 -0800

    commit

---
+++ Day 07/homework/lesson7.py
@@ -0,0 +1,7 @@
+num1 = int(input("enter your number 1: "))
+num2 = int(input("enter your number 2: "))
+num3 = int(input("enter your number 3: "))
+print(num1 + num2 + num3)
+print(num1 - num2 - num3)
+print(num1 * num2 * num3)
+print(num1 / num2 / num3)
