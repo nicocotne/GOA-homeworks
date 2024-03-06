894904307a3e67d268e131e01dadbffa6e0b1d41
nicocotne <nicoocingm@gmail.com>
2024-03-06 08:32:22 -0800

    commit

---
+++ Day 10/homework/homework.py
@@ -0,0 +1,34 @@
+#დავ. 1
+for num in range(0,21):
+    print(num)
+
+#დავ. 2
+i = int(input("pleas enter your number: "))
+ii = int(input("pleas enter your number: "))
+for i in range(0,50):
+    print(i)
+#დავ. 3
+for num in range(50,101):
+    print(num)
+
+
+#დავ. 4
+for num in range(50,101,-1):
+    print(num)
+
+
+#დავ. 5
+for i in range(51):
+    print(i+i)
+
+
+#დავ. 6
+num = int(input("pleas enter your number: "))
+for num in range(50):
+    print(num)
+
+#დავ. 7
+ageinnow = int(input("pleas enter your age: "))
+agefromnow= int(input("pleas enter your age with 10 years in the future: "))
+for i in range(10,30):
+    print(i)
