07e00e8c331125129454680805f11a753a74130c
nicocotne <nicoocingm@gmail.com>
2024-04-02 23:08:41 -0700

    commit

---
+++ Day 18/classwork/classwork.py
@@ -0,0 +1,4 @@
+numbers = list(range(1, 11))
+nums = [num for num in numbers 
+                if num % 2 == 0]
+print(nums)
---
+++ Day 18/homework/homewrok.py
@@ -0,0 +1,68 @@
+#დავ 0.0
+My_name = "Cotne"
+indexes_string =""
+for i in range(len(My_name)):
+    if i % 2 == 0:
+        indexes_string += My_name[i]
+print(indexes_string)
+#დავ 0.0
+name = "Basa"
+indexes_string =""
+for i in range(len(My_name)):
+    if i % 2 == 0:
+        indexes_string += My_name[i]
+print(indexes_string)
+#დავ 0.1
+name = "goblin"
+indexes_string =""
+for i in range(len(My_name)):
+    if i % 2 == 0:
+        indexes_string += My_name[i]
+print(indexes_string)
+#დავ 0.2
+name = "Thor"
+indexes_string =""
+for i in range(len(My_name)):
+    if i % 2 == 0:
+        indexes_string += My_name[i]
+print(indexes_string)
+#დავ 0.3
+name = "Hubert Blaine Wolfeschlegelsteinhausenbergerdorff Sr."
+indexes_string =""
+for i in range(len(My_name)):
+    if i % 2 == 0:
+        indexes_string += My_name[i]
+print(indexes_string)
+#დავ 0.4
+name_surname = "Albert Einstein"
+indexes_string =""
+for i in range(len(My_name)):
+    if i % 2 == 0:
+        indexes_string += My_name[i]
+print(indexes_string)
+
+
+#დავ 1.0
+number = 34
+number += 2
+number -= 7
+number *= 5
+number /= 3
+number %= 1
+print(number)
+#დავ 1.1
+number = 50000
+number += 3333
+number -= 750
+number *= 600
+number /= 60
+number %= 50
+print(number)
+#დავ 1.2
+number = 6
+number += 5
+number -= 4
+number *= 3
+number /= 2
+number %= 1
+print(number)
